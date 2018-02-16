353c17ef12b03ab491eaf8b7e6d1014cd30bcfab
Fernando Ochoa <heavyvinicio@gmail.com>
2018-02-16 08:23:27 -0500

    init

--- jtable-swing-util/src/main/java/io/util/swing/jtable/reflection/ReflectionUtil.java
+++ jtable-swing-util/src/main/java/io/util/swing/jtable/reflection/ReflectionUtil.java
@@ -79,6 +79,21 @@ public final class ReflectionUtil implements Serializable {
         return new ArrayList<>(columnNames.values());
     }
 
+    public static <T> List getColumnNames(T object, int... columns) {
+        Map columnNames = new TreeMap();
+        for (Field field : getFieldsObject(object)) {
+            if (isStaticOrFinal(field)) {
+                continue;
+            }
+            if (field.isAnnotationPresent(TableColumn.class) && ((TableColumn) field.getDeclaredAnnotation(TableColumn.class)).visible() && !"DEFAULT".equalsIgnoreCase(field.getDeclaredAnnotation(TableColumn.class).columnName())) {
+                if (validateColumnOrder(field.getDeclaredAnnotation(TableColumn.class).order(), columns)) {
+                    columnNames.put(field.getDeclaredAnnotation(TableColumn.class).order(), field.getDeclaredAnnotation(TableColumn.class).columnName());
+                }
+            }
+        }
+        return new ArrayList<>(columnNames.values());
+    }
+
     /**
      * <b> Method for convert list objects to map. </b>
      * <p>
@@ -98,6 +113,52 @@ public final class ReflectionUtil implements Serializable {
         for (T object : dataList) {
             try {
                 rows.put(index, convertObjectData2Map(object));
+                index++;
+            } catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+                return new TreeMap<>();
+            }
+        }
+        return rows;
+    }
+
+    /**
+     * <b> Method for convert list objects to map. </b>
+     * <p>
+     * [Author: pocho, Date: 14/09/2017]
+     * </p>
+     *
+     * @param dataList
+     *            lista de datos
+     * @return mapa de filas y columnas
+     */
+    public static <T> Map<Integer, Map<Integer, Object>> convertListData2Map(List<T> dataList, String[] attributes) {
+        Map<Integer, Map<Integer, Object>> rows = new TreeMap<>();
+        int index = 0;
+        if (dataList == null || dataList.isEmpty()) {
+            return new TreeMap<>();
+        }
+        for (T object : dataList) {
+            try {
+                rows.put(index, convertObjectData2Map(object, attributes));
+                index++;
+            } catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+                return new TreeMap<>();
+            }
+        }
+        return rows;
+    }
+
+    public static <T> Map<Integer, Map<Integer, Object>> convertListData2Map(List<T> dataList, int... columnas) {
+        Map<Integer, Map<Integer, Object>> rows = new TreeMap<>();
+        int index = 0;
+        if (dataList == null || dataList.isEmpty()) {
+            return new TreeMap<>();
+        }
+        for (T object : dataList) {
+            try {
+                rows.put(index, convertObjectData2Map(object, columnas));
                 index++;
             } catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
                 Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
@@ -141,6 +202,71 @@ public final class ReflectionUtil implements Serializable {
             }
         }
         return row;
+    }
+
+    /**
+     * <b> Method for convert object to map (row in {@link JTable}).
+     * </b>
+     * <p>
+     * [Author: pocho, Date: 14/09/2017]
+     * </p>
+     *
+     * @param object
+     *            T
+     * @return {@link Map}
+     * @throws IntrospectionException
+     *             error
+     * @throws IllegalAccessException
+     *             error
+     * @throws IllegalArgumentException
+     *             error
+     * @throws InvocationTargetException
+     *             error
+     */
+    private static <T> Map<Integer, Object> convertObjectData2Map(T object, String[] attributes) throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Map<Integer, Object> row = new TreeMap<>();
+        for (int i = 0; i < attributes.length; i++) {
+            for (Field field : getFieldsObject(object)) {
+                if (isStaticOrFinal(field)) {
+                    continue;
+                }
+                if (attributes[i].equals(field.getName())) {
+                    row.put(i, loadValueColumn(object, field));
+                    break;
+                }
+            }
+        }
+        return row;
+    }
+
+    private static <T> Map<Integer, Object> convertObjectData2Map(T object, int... columns) throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        int order = 0;
+        Map<Integer, Object> row = new TreeMap<>();
+        for (Field field : getFieldsObject(object)) {
+            if (isStaticOrFinal(field)) {
+                continue;
+            }
+            TableColumn tableColumn = null;
+            if (field.isAnnotationPresent(TableColumn.class)) {
+                tableColumn = field.getDeclaredAnnotation(TableColumn.class);
+            }
+            if (tableColumn != null && tableColumn.visible()) {
+                if (validateColumnOrder(tableColumn.order(), columns)) {
+                    row.put(order, loadValueColumn(object, field));
+                    order++;
+                }
+            }
+        }
+        return row;
+    }
+
+    private static boolean validateColumnOrder(int orden, int... columns) {
+        for (int i = 0; i < columns.length; i++) {
+            if (columns[i] == orden) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -216,10 +342,12 @@ public final class ReflectionUtil implements Serializable {
             if (Modifier.isPrivate(field.getModifiers()) && field.isAnnotationPresent(TableColumn.class)) {
                 TableColumn tableColumn = field.getDeclaredAnnotation(TableColumn.class);
                 try {
-                    Object valorAnterior = loadValueColumn(object, field);
-                    Object valorNuevo = values.get(index);
-                    if (tableColumn.editable() && (null == valorAnterior || !valorAnterior.equals(valorNuevo))) {
-                        changeValue(object, field, valorNuevo);
+                    if (tableColumn.editable()) {
+                        Object valorAnterior = loadValueColumn(object, field);
+                        Object valorNuevo = values.get(index);
+                        if (tableColumn.editable() && (null == valorAnterior || !valorAnterior.equals(valorNuevo))) {
+                            changeValue(object, field, valorNuevo);
+                        }
                     }
                 } catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
                     Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
@@ -228,6 +356,62 @@ public final class ReflectionUtil implements Serializable {
                 }
             }
             index++;
+        }
+    }
+
+    public static <T> void mergeChanges(T object, Map<Integer, Object> values, final Map<Integer, String> columnas) {
+        for (Field field : getFieldsObject(object)) {
+            if (Modifier.isPrivate(field.getModifiers())) {
+                try {
+                    for (Map.Entry<Integer, String> item : columnas.entrySet()) {
+                        if (field.getName().equals(item.getValue())) {
+                            Object valorAnterior = loadValueColumn(object, field);
+                            Object valorNuevo = values.get(item.getKey());
+                            if (null == valorAnterior || !valorAnterior.equals(valorNuevo)) {
+                                changeValue(object, field, valorNuevo);
+                            }
+                        }
+                    }
+                } catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                    Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (Exception ex) {
+                    Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    /**
+     * <p>
+     * Method for make merge two object when the case is necesary
+     * </p>
+     *
+     * @param <T>
+     * @param object
+     *            OBject
+     * @param values
+     *            row to validate
+     */
+    public static <T> void mergeChanges(T object, Map<Integer, Object> values, int... columns) {
+        int index = 0;
+        for (Field field : getFieldsObject(object)) {
+            if (Modifier.isPrivate(field.getModifiers()) && field.isAnnotationPresent(TableColumn.class)) {
+                TableColumn tableColumn = field.getDeclaredAnnotation(TableColumn.class);
+                try {
+                    if (tableColumn.editable() && validateColumnOrder(tableColumn.order(), columns)) {
+                        Object valorAnterior = loadValueColumn(object, field);
+                        Object valorNuevo = values.get(index);
+                        if (tableColumn.editable() && (null == valorAnterior || !valorAnterior.equals(valorNuevo))) {
+                            changeValue(object, field, valorNuevo);
+                            index++;
+                        }
+                    }
+                } catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                    Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (Exception ex) {
+                    Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
     }
 
